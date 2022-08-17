079f4888db0a3b9d2431ec1c9e981925360f772d
CalebeSG <calebeguimaraes10@gmail.com>
2022-08-17 09:20:15 -0300

    created profile component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,22 @@
-import { Container } from './styles'
+import { useMemo } from 'react'
+
+import emojis from '../../shared/emojis'
+import { Container, Profile, Welcome, UserName } from './styles'
 
 const Header: React.FC = () => {
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * emojis.length)
+    return emojis[index]
+  }, [])
+
   return (
     <Container>
       <h1>Header</h1>
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <UserName>Calebe Guimarães</UserName>
+      </Profile>
     </Container>
   )
 }
